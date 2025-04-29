a9e3d66575cf07a6d3b5a4547c47f19439f9268f
Yunus Emre Kaymak <yunusemrekaymak9@gmail.com>
2025-04-29 23:00:09 +0300

    settings.py düzenlemesi 2

--- SPOTIFY/settings.py
+++ SPOTIFY/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'corsheaders',
 
 ]
 
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'SPOTIFY.urls'
