dfdafdfc6a6924769a2ee64d83c86d3d082c10f6
yemrekaymak <yunsemrekaymak9@gmail.com>
2025-05-05 23:02:55 +0300

    oldu herhalde041

--- app/urls.py
+++ app/urls.py
@@ -8,10 +8,6 @@ from .views import (
     get_user_data,
     sanatci_listesi,
     add_to_queue,
-    search_tracks,
-    search_and_add_track,  # Yeni eklenen view
-    get_user_playlists,    # Yeni eklenen view
-    add_track_to_playlist, # Yeni eklenen view
 )
 
 urlpatterns = [
@@ -23,8 +19,4 @@ urlpatterns = [
     path("user/", get_user_data, name='get_user_data'),
     path("sanatci_listesi/", sanatci_listesi, name='sanatci_listesi'),
     path("add_to_queue/", add_to_queue, name='add_to_queue'),
-    path("search_tracks/", search_tracks, name='search_tracks'),
-    path("search_and_add_track/", search_and_add_track, name='search_and_add_track'),
-    path("get_user_playlists/", get_user_playlists, name='get_user_playlists'),
-    path("add_track_to_playlist/", add_track_to_playlist, name='add_track_to_playlist'),
 ]
